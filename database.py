9264674caeda385f17d2e72086b8ec409681bace
antifly55 <antifly55@kookmin.ac.kr>
2023-10-27 22:40:41 +0900

    add: DB configurate

--- database.py
+++ database.py
@@ -7,13 +7,14 @@ properties.read('config.ini')
 properties_DB = properties['DB']
 
 HOST = properties_DB['host']
+PORT = properties_DB['port']
 USER = properties_DB['user']
 PASSWORD = properties_DB['password']
 DB = properties_DB['db']
 CHARSET = properties_DB['charset']
 
 
-conn = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DB, charset=CHARSET)
+conn = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB, charset=CHARSET)
 
 def get_db():
     db = conn.cursor()
