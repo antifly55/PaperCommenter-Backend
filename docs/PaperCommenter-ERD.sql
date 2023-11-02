174b92d5f1c66d1294119c0443f60e468bc24eea
antifly55 <antifly55@kookmin.ac.kr>
2023-11-02 13:41:26 +0900

    add: DDL Auto Increment ID

--- docs/PaperCommenter-ERD.sql
+++ docs/PaperCommenter-ERD.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE `PAPER` (
-	`id`	int	NOT NULL,
+	`id`	int	NOT NULL	AUTO_INCREMENT PRIMARY KEY,
 	`user_id`	int	NOT NULL,
 	`slug`	nvarchar(255)	NOT NULL,
 	`title`	nvarchar(255)	NOT NULL,
@@ -15,7 +15,7 @@
 );
 
 CREATE TABLE `COMMENT` (
-	`id`	int	NOT NULL,
+	`id`	int	NOT NULL	AUTO_INCREMENT	PRIMARY KEY,
 	`paper_id`	int	NOT NULL,
 	`user_id`	int	NOT NULL,
 	`hashed_identifier`	varchar(255)	NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE `COMMENT` (
 );
 
 CREATE TABLE `USER` (
-	`id`	int	NOT NULL,
+	`id`	int	NOT NULL	AUTO_INCREMENT	PRIMARY KEY,
 	`username`	nvarchar(255)	NOT NULL,
 	`hashed_password`	varchar(255)	NOT NULL,
 	`email`	nvarchar(255)	NOT NULL,
@@ -43,31 +43,6 @@ CREATE TABLE `PAPER_LIKE` (
 CREATE TABLE `COMMENT_LIKE` (
 	`comment_id`	int	NOT NULL,
 	`user_id`	int	NOT NULL
-);
-
-ALTER TABLE `PAPER` ADD CONSTRAINT `PK_PAPER` PRIMARY KEY (
-	`id`,
-	`user_id`
-);
-
-ALTER TABLE `COMMENT` ADD CONSTRAINT `PK_COMMENT` PRIMARY KEY (
-	`id`,
-	`paper_id`,
-	`user_id`
-);
-
-ALTER TABLE `USER` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `PAPER_LIKE` ADD CONSTRAINT `PK_PAPER_LIKE` PRIMARY KEY (
-	`paper_id`,
-	`user_id`
-);
-
-ALTER TABLE `COMMENT_LIKE` ADD CONSTRAINT `PK_COMMENT_LIKE` PRIMARY KEY (
-	`comment_id`,
-	`user_id`
 );
 
 ALTER TABLE `PAPER` ADD CONSTRAINT `FK_USER_TO_PAPER_1` FOREIGN KEY (
