26f86d5a1f54d79d9ec60711f06d0abde9c34fd0
antifly55 <antifly55@kookmin.ac.kr>
2023-11-17 00:53:08 +0900

    fix: CommentApp CRUD

--- core/CommentApp/crud.py
+++ core/CommentApp/crud.py
@@ -1,15 +1,12 @@
 from datetime import datetime
 
-import hashlib
-
 from pymysql.cursors import Cursor
 
-
-def make_hashed_identifier(identifier):
-    return hashlib.md5(f"-".join(identifier).encode()).hexdigest()
+from common.utils import hash_for_identification
 
 def get_comment_by_hashed_identifier(db: Cursor,
                                      hashed_identifier: str):
+    
     db.execute(f"SELECT * FROM comment WHERE hashed_identifier='{hashed_identifier}'")
     comment = db.fetchone()
 
@@ -38,7 +35,7 @@ def create_comment(db: Cursor,
                    content: str):
 
     now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    hashed_identifier = make_hashed_identifier(identifier=[paper_id, user_id, username, content, now])
+    hashed_identifier = hash_for_identification(f'{user_id}-{now}')
 
     # Auto Increment ID
     comment_info = "(paper_id, user_id, hashed_identifier, username, content, create_datetime, like_count)"
@@ -50,18 +47,22 @@ def create_comment(db: Cursor,
 def update_comment(db: Cursor,
                    comment_id: int,
                    content: str):
+    
     now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
     db.execute(f"UPDATE comment SET content='{content}', modify_datetime='{now}' WHERE id={comment_id}")
     db.connection.commit()
 
 def delete_comment(db: Cursor,
                    comment_id: int):
+    
     db.execute(f"DELETE FROM comment WHERE id={comment_id}")
     db.connection.commit()
 
 def get_comment_like(db: Cursor,
                    comment_id: int,
                    user_id: int):
+    
     db.execute(f"SELECT * FROM comment_like WHERE comment_id={comment_id} and user_id={user_id}")
     paper_like = db.fetchone()
 
@@ -70,9 +71,13 @@ def get_comment_like(db: Cursor,
 def like_comment(db: Cursor,
                comment_id: int,
                user_id: int):
+    
     db.execute(f"INSERT INTO comment_like (comment_id, user_id) VALUES ({comment_id}, {user_id})")
+    db.connection.commit()
 
 def withdraw_like_comment(db: Cursor,
                         comment_id: int,
                         user_id: int):
+    
     db.execute(f"DELETE FROM comment_like WHERE comment_id={comment_id} and user_id={user_id}")
+    db.connection.commit()
