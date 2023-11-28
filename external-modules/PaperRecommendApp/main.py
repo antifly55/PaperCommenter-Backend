029c7a5288ea5e3f6ce0b18f5ccffb54aa3559f8
antifly55 <antifly55@kookmin.ac.kr>
2023-11-28 16:52:33 +0900

    add: scaffold for PaperRecommendApp

--- external-modules/PaperRecommendApp/main.py
+++ external-modules/PaperRecommendApp/main.py
@@ -1,5 +1,7 @@
 # TODO: implement PaperRecommendApp with Collaborative Filtering, Airflow
 
+import torch
+import torch.nn.functional as F
 import numpy as np
 
 from database import execute_select_sql
@@ -12,23 +14,51 @@ def main():
     # preprocess
     paper_indices = []
     user_indices = []
-    score_dataset = []
+
+    papers = []
+    users = []
+    ratings = []
 
     for row in query_results:
-        paper_id, user_id, score = row['paper_id'], row['user_id'], row['rating']
+        paper_id, user_id, rating = row['paper_id'], row['user_id'], row['rating']
 
         if paper_id not in paper_indices.keys():
             paper_indices[paper_id] = len(paper_indices)
         if user_id not in user_indices.keys():
             user_indices[user_id] = len(user_indices)
 
-        score_dataset.append((paper_id, user_id, score))
+        papers.append(paper_indices[paper_id])
+        users.append(user_indices[user_id])
+        ratings.append(rating)
+
+    ## convert types
+    ## papers, users, ratings to torch.Long/FloatTensor
 
     # make matrix
     features = 10
-    P = np.zeros(len(user_indices), features)
-    Q = np.zeros(len(paper_indices), features)
+    P = np.zeros(len(users), features, requires_grad=True)
+    Q = np.zeros(len(papers), features, requires_grad=True)
 
-    
+    # train default MF
+    optimizer = torch.optim.Adam([P, Q], lr=0.1)
 
-    pass
+    for epoch in range(100):
+        hypothesis = torch.sum(P[papers], Q[users], dim=1)
+        cost = F.mse_loss(hypothesis, ratings)
+
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+
+        if epoch % 100 == 0:
+            print(f"epoch: {epoch}, cost: {round(cost.item(), 4)}")
+
+    # test
+    ## convert types
+    papers_test = []
+    users_test = []
+
+    # get ratings of papers_test & users_test
+    with torch.no_grad():
+        hypo_test = torch.sum(P[papers_test], Q[users_test], dim=1)
+        
